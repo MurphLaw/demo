32a2d856bcebe40c70f6f715ab9f7fa2eedb0b8e
joan.caicedo <joan.caicedo@supergiros.com.co>
2024-02-28 17:12:53 -0500

    Se realiza la data de pruebas para pasar de parametro en el screen de betCar

---
+++ lib/models/bet_car_model.dart
@@ -0,0 +1,11 @@
+class BetCar {
+
+  BetCar({required this.betNumber, required this.mixValue, 
+  required this.directValue, required this.iconBets});
+
+  String betNumber;
+  double mixValue;
+  double directValue;
+  List<String> iconBets;
+
+}
--- lib/screens/bet_cart.dart
+++ lib/screens/bet_cart.dart
@@ -1,18 +1,15 @@
-import 'package:demo/widgets/bets/bet_widget.dart';
-import 'package:demo/widgets/bets/tittle_chance.dart';
 import 'package:demo/widgets/custom_multicolor_message.dart';
-import 'package:demo/widgets/text_write_your_numbers.dart';
 import 'package:flutter/material.dart';
+import '../models/bet_car_model.dart';
 
-import '../models/product_model.dart';
-import '../widgets/commons/bet_to_your_luck.dart';
+
 import '../widgets/commons/custom_app_bar_1.dart';
-import '../widgets/custom_decorative_2.dart';
 
 class BetCarScreen extends StatelessWidget {
 
-  BetCarScreen({Key? key}) : super(key: key);
+  BetCarScreen({Key? key, required this.betCarModel}) : super(key: key);
   
+  BetCar betCarModel;
 
   @override
   Widget build(BuildContext context) {
@@ -32,14 +29,12 @@ class BetCarScreen extends StatelessWidget {
               ), 
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children:  [
+                children:  const [
                   CustomMultiColorMessage(text1: 'Carrito de apuestas', text2: '', fontText1: 18, fontText2: 18, 
                   colorText1: Color.fromARGB(255, 0, 0, 0), colorText2: Color.fromARGB(255, 0, 0, 0), 
                   marginVertical: 0, marginHorizontal: 0, 
                   colorBackground: Color.fromARGB(255, 255, 255, 255), 
                   containerWidth: 200, containerHeight: 50),
-
-                  
                   ],
               ),
             ))
