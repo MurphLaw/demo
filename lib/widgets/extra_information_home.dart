9440e0c5bd275d648ec4fc144e3d66d13e97fd9d
Kev407 <karistizabal307@gmail.com>
2024-02-28 09:21:13 -0500

    extra information widget

--- lib/widgets/extra_information_home.dart
+++ lib/widgets/extra_information_home.dart
@@ -1,3 +1,4 @@
+import 'package:demo/models/extra_information_home_list.dart';
 import 'package:flutter/material.dart';
 
 class ExtraInformationHome extends StatelessWidget {
@@ -5,24 +6,44 @@ class ExtraInformationHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        alignment: Alignment.bottomCenter,
-        height: 150,
-        child: Padding(
-            padding: const EdgeInsets.all(12), //Tamaño del margen
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 4,
-              itemBuilder: (context, count) {
-                return Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                      width: 200,
-                      height: 65,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.rectangle, color: Colors.blue)),
-                );
-              },
-            )));
+    return Column(children: <Widget>[
+      Container(
+          alignment: Alignment.bottomCenter,
+          height: 150,
+          child: Padding(
+              padding: const EdgeInsets.all(12), //Tamaño del margen
+              child: PageView.builder(
+                controller: PageController(viewportFraction: 0.7),
+                scrollDirection: Axis.horizontal,
+                itemCount: imageExtraInformation.length,
+                itemBuilder: (context, count) {
+                  return Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                        width: 200,
+                        height: 65,
+                        decoration: BoxDecoration(
+                            // shape: BoxShape.rectangle, color: Colors.blue)),
+                            image: DecorationImage(
+                          image: AssetImage(imageExtraInformation[count].image),
+                          // fit: BoxFit.cover,
+                        )),
+                      ));
+                },
+              ))),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 8.0,
+            height: 8.0,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          )
+        ],
+      )
+    ]);
   }
 }
