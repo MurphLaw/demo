90ec2305fa586a96ae23fbb606331010ba69645f
MiguelPineda <miguel.pineda@supergiros.com.co>
2024-02-28 10:33:57 -0500

    hola 2

--- lib/screens/main_bet_screen.dart
+++ lib/screens/main_bet_screen.dart
@@ -1,11 +1,8 @@
 import 'package:demo/mocks/acomulado.dart';
 import 'package:demo/models/acomulado.dart';
-import 'package:demo/widgets/account_money.dart';
 import 'package:demo/widgets/commons/account_money.dart';
 import 'package:demo/widgets/custom_decorative_4.dart';
 import 'package:demo/widgets/generic_element_bottom_bar.dart';
-import 'package:demo/widgets/main_app_bar.dart';
-import 'package:demo/widgets/main_bottom_bar.dart';
 import 'package:demo/widgets/movie_slider.dart';
 import 'package:demo/widgets/commons/main_app_bar.dart';
 import 'package:demo/widgets/commons/main_bottom_bar.dart';
@@ -16,8 +13,6 @@ import '../widgets/commons/bet_to_your_luck.dart';
 
 class MainBetScreen extends StatelessWidget {
 
-  final total = ['Hola', 'mundo', 'soy'];
-  final logos = ['assets/images/Grupo 8059.png', 'assets/images/Grupo 8068.png', 'assets/images/Grupo 8053.png'];
   const MainBetScreen({super.key});
   
 
@@ -43,11 +38,11 @@ class MainBetScreen extends StatelessWidget {
                 'Recarga',
                 style: TextStyle(color: Colors.white),
               ),
-            ],
+            
             const SizedBox(height: 18,),
             Image.asset('assets/images/dollar.png', color: Colors.white,),
             const Text('Recarga',style:  TextStyle(color: Colors.white),),
-        ],
+          ],
           )
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -60,7 +55,8 @@ class MainBetScreen extends StatelessWidget {
           const SizedBox(
             height: 8,
           ),
-          MovieSlider(lista: lista, 
+          MovieSlider(
+            lista: lista, 
           ),
           const SizedBox(
             height: 8,
